c499723ca9cc0135f4f584850292128035b04736
Ahmed Kamal <kamala@cardiff.ac.uk>
2019-12-04 12:38:13 +0000

    Merge with sync works!

--- interleaving.py
+++ interleaving.py
@@ -1,10 +1,13 @@
-#pivectors[146] =[0,0,1,0,0,1,1,1,1,0,1,0,1,0,1,0,0,1,0,0,0,1,0,0,0,1,1,0,0,1,1,1,1,0,0,1,1,1,1,1,0,0,1,1,0,1,1,1,1,0,1,0,1,1,0,1,1,0,1,0,0,0,0,0,1,1,1,1,1,0,1,0,1,0,0,0,0,0,1,1,1,1,1,0,1,0,0,1,0,0,
-#                1,0,1,0,0,0,0,1,0,0,1,1,0,0,0,0,0,1,1,0,0,0,0,1,1,0,0,1,1,1,
-#                0,1,1,1,0,1,1,0,1,0,1,0,1,0,0,0,0,1,1,1,0,0,0,0,1,1]
 PI4Vectors = [0,0,1,0,0,1,1,1,1,0,1,0,1,0,1,0,0,1,0,0,0,1,0,0,0,1,1,0,0,1,1,1,1,0,0,1,1,1,1,1,0,0,1,1,0,1,1,1,1,0,1,0,1,1,0,1,1,0,1,0,0,0,0,0,1,1,1,1,1,0,1,0,1,0,0,0,0,0,1,1,1,1,1,0,1,0,0,1,0,0,1,0,1,0,0,0,0,1,0,0,1,1,0,0,0,0,0,1,1,0,0,0,0,1,1,0,0,1,1,1,0,1,1,1,0,1,1,0,1,0,1,0,1,0,0,0,0,1,1,1,0,0,0,0,1,1]
+convenc_check = '1 1 0 1 1 0 0 1 1 1 1 1 1 1 0 0 0 0 0 0 0 1 0 0 0 1 1 0 0 1 0 1 1 0 0 0 1 0 1 1 1 0 1 0 0 0 1 0 1 0 1 0 1 1 1 1 1 1 1 0 1 0 1 1 0 0 0 1 1 1 1 0 1 0 0 1 0 0 1 0 1 1 1 1 1 0 0 1 0 1 0 0 1 1 1 1 0 1 0 0 0 1 0 1 0 1 0 0 1 0 0 0 0 0 0 1 1 1 0 1 1 0 1 1 0 1 1 0 1 0 1 0 1 1 1 0 1 1 1 1 1 1 0 0 0 0'
+pichar = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ /'   #this is line assigns a value to each 
+                                                    #character within the array
 
-pichar = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ /'
-#print(pichar.index('G is', 16))
+message = "OZ7IGY  "
+# Check message length
+
+# Source Encoding
+
 o = pichar.find("O")
 z = pichar.find("Z")
 x = pichar.find("7")
@@ -13,8 +16,8 @@ g = pichar.find("G")
 y = pichar.find("Y")
 s = pichar.find(" ")
  
-SourceEnc = o 
-SourceEnc = SourceEnc*38 + z
+SourceEnc = o                           #This is the method used to generate the 
+SourceEnc = SourceEnc*38 + z            #Source encoding of the bits
 SourceEnc = SourceEnc*38 + x
 SourceEnc = SourceEnc*38 + i 
 SourceEnc = SourceEnc*38 + g
@@ -22,7 +25,7 @@ SourceEnc = SourceEnc*38 + y
 SourceEnc = SourceEnc*38 + s
 SourceEnc = SourceEnc*38 + s
 print(s)
-print(SourceEnc)
+print(SourceEnc)                        #this line prints out the Source encoding
 hexa = hex(SourceEnc)
 print("hex decimal equivlant is:", hexa)
 
@@ -40,7 +43,7 @@ def parity(value):
     return even 
 
 def printdata(title, dat):
-    print('%s' % title)     #(4*30 + 26)\n
+    print('%s' % title)     
     print(" ")
 
     count = len(title) + 9
@@ -50,108 +53,85 @@ def printdata(title, dat):
 
     for i in range (0, 145):
         print(dat, end=" ")
-        #if ((i+1)%30 == 0):
-            #print("/n")
+        
     print("\n\n")
         
 
-
-
-Poly1 = 0xF2D05351
+Poly1 = 0xF2D05351               #generating two registers named poly1 and poly2
 Poly2 = 0xE4613C47
 N = 0
 I = 0
-    
-#asciisymbols = 146       
-#SourceEnc = 0
-#ConvEnc = [0] * 146
 ConvEnc = []
 print("empty list is", ConvEnc)
-#np1 = N & Poly1
-#np2 = N & Poly2
 
-
-for j in range(0, 73):
-    N = N << 1
+for j in range(0, 73):                       #line 63-72 demostrates how the convolutional 
+    N = N << 1                               #encoding was achieved
     if (SourceEnc & 0x20000000000) != 0:
         N = N | 1
         
     SourceEnc <<= 1
-    #j += 1
     
-    #ConvEnc = parity(N & Poly1)
-    #CE1 = ConvEnc
     ConvEnc.append(parity(N & Poly1))
-    #print("ConvEnc1 :", ConvEnc)
-
-    #ConvEnc = parity(N & Poly2)
-    #CE2 = ConvEnc
+    
     ConvEnc.append(parity(N & Poly2))
-    #print("ConvEnc2 is:", ConvEnc)
+   
+print("ConvEnc is:", ConvEnc)    #this lines prints out the convolutional 
+                                           #encoding array
+convenc_check = convenc_check.split()
+convenc_check = [int(i) for i in convenc_check]
+print("Conv check is", convenc_check)
+assert(ConvEnc == convenc_check)
 
-    #ConvEnc = CE1 + CE2
- 
-print("ConvEnc is:", ConvEnc, end=" ")
 
 # interleaving encoding
-import array
-def parity2(value2):
-    for bitno in range (0, 8):
-        if (((i >> bitno) & 0x01) == 0x01):
-            value2 |= 1 << (7 - bitno)
-        else:
-            value2 &= ~(1 << (7 - bitno))
-    return value2
 
-P = 0 
-R = 0
+P = 0                          #initialising a counter P
+R = 0                          #initialising a counter R 
 PI4symbols = 146 
-interleaved = [ ]
 R_array = [ ]
+R_array_other = [ ]                  
 
-for I in range (0, 256):
-    for BitNo in range (0, 8):
-
-
-
+for I in range (0, 256):                      #line 84-97 demonstrates how to achieve 
+    for BitNo in range (0, 8):                #the interleaving encoding
         if (((I >> BitNo) & 0x01) == 0x01):
             R |= 1 << (7 - BitNo)
         else:
             R = R & ~(1 << (7 - BitNo))
-    
-   
-    
-    if ((P < PI4symbols) and (R < PI4symbols)):
-   
 
-       
-        
+    R_other = int('{:08b}'.format(I)[::-1], 2)  
+    assert(R == R_other, "Rs do not match ")
+    #if ((P < PI4symbols) and (R < PI4symbols)):
+    if R < 146:
         R_array.append(R)
+    if R_other < 146:
+        R_array_other.append(R_other)
        
-        
+print("The equivlent R array is:", R_array)
+print("R_other is ", R_array_other)
+#print("R is", R)
 
-       
-   
-    print("The equivlent R array is:", R_array)
-
+incrementing = 0
+interleaved = [0]*PI4symbols             #defining an array for the interleaved data
 for index in R_array:
-    interleaved.append(ConvEnc[index])
-print("Convolutional encoding data is:", ConvEnc)
-print("interleaved data is", interleaved)
-    
-    
+    interleaved[index] = ConvEnc[incrementing]
+    incrementing += 1
+
+print("Convolutional encoding data is:", ConvEnc)      #prints the CE array
+print("interleaved data is", interleaved)              #prints out the interleaved array
 print("length of conv encoding array:", len(ConvEnc))
 print("length of interleaving data is:", len(interleaved))
 print("I is:", I)
 print("R is:", R)
 
 
-new_interleaved = [i * 2 for i in interleaved]
-print("double interleaved:", new_interleaved)
+
+
+
+#merge with sync
 
 merge_sync = [ ] 
 for i in range(0, len(interleaved)): 
-    merge_sync.append(new_interleaved[i] + PI4Vectors[i])
+    merge_sync.append(interleaved[i] * 2 + PI4Vectors[i])
 
 print("data symbols is:", merge_sync ) 
 
