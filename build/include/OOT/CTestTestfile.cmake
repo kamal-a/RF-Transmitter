714b9df357b0bf4a4d87b18e862dfc476d195961
Derek Kozel <derek@bitstovolts.com>
2020-05-24 16:17:09 +0100

    Removed build directory

--- build/include/OOT/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /root/gr-OOT/include/OOT
-# Build directory: /root/gr-OOT/build/include/OOT
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- build/lib/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /root/gr-OOT/lib
-# Build directory: /root/gr-OOT/build/lib
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
